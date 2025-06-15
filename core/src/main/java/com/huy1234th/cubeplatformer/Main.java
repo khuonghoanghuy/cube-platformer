b5dc67b570eb5a609106cec0c6a61134e58afd9a
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2025-06-15 15:18:31 +0700

    player are now moving

--- core/src/main/java/com/huy1234th/cubeplatformer/Main.java
+++ core/src/main/java/com/huy1234th/cubeplatformer/Main.java
@@ -1,41 +1,62 @@
 package com.huy1234th.cubeplatformer;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private Texture player;
+    // all players config
+    private Texture playerTexture;
+    private Sprite playerSprite;
+    float playerSpeed = 100f;
+
+    // all objects thingie
     private SpriteBatch spriteBatch;
     
     @Override
     public void create() {
-        player = new Texture("player.png");
+        playerTexture = new Texture("player.png");
+        playerSprite = new Sprite(playerTexture);
+
         spriteBatch = new SpriteBatch();
     }
 
     @Override
     public void render() {
         draw();
+        inputGame();
     }
 
     private void draw() {
-        ScreenUtils.clear(Color.BLACK);
+        ScreenUtils.clear(Color.GRAY);
 
         // begin the sprite draw
         spriteBatch.begin();
-
-        spriteBatch.draw(player, 1, 1);
+        
+        playerSprite.draw(spriteBatch);
 
         // after that, end the sprite draw
         spriteBatch.end();
     }
 
+    private void inputGame() {
+        float delta = Gdx.graphics.getDeltaTime();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            playerSprite.translateX(playerSpeed * delta);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            playerSprite.translateX(-playerSpeed * delta);
+        }
+    }
+
     @Override
     public void dispose() {
-        player.dispose();
+        // player.dispose();
     }
 }
